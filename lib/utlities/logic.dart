7efbb0600311159d5e3ffada5c4b2c96446d3bf2
whymohammad <muhammadbangash289@gmail.com>
2022-01-19 05:12:07 -0800

    commit

---
+++ lib/utlities/logic.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Logic {
+
+
+static  bool isGradient = false;
+static double shadowRadius = 0.0;
+static double shadowDx = 0.0;
+static double shadowDy = 0.0;
+static Color shadowColors = Colors.red;
+}
